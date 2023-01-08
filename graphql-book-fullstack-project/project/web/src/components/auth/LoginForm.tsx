d036bb096e966346833db32713f4d663805bbe50
bulhwi <mm225012@gmail.com>
2023-01-08 23:49:56 +0900

    login UI

--- graphql-book-fullstack-project/project/web/src/components/auth/LoginForm.tsx
+++ graphql-book-fullstack-project/project/web/src/components/auth/LoginForm.tsx
@@ -11,17 +11,35 @@ import {
   useColorModeValue
 } from "@chakra-ui/react";
 import {useForm} from "react-hook-form";
-import {LoginMutationVariables} from "../../generated/graphql";
+import {LoginMutationVariables, useLoginMutation} from "../../generated/graphql";
+import {useNavigate} from "react-router-dom";
 
 function RealLoginForm(): React.ReactElement {
+  const navigate = useNavigate();
+  const [login, {loading}] = useLoginMutation();
+
   const {
     register,
     handleSubmit,
     formState: {errors},
+    setError,
   } = useForm<LoginMutationVariables>();
 
-  const onSubmit = (formData: LoginMutationVariables) => {
-    console.log(formData);
+  // login
+  const onSubmit = async (formData: LoginMutationVariables) => {
+    const {data} = await login({variables: formData});
+    if(data?.login.errors) {
+      data.login.errors.forEach(err => {
+        const field = `loginInput.${err.field === 'password' ? 'password' : 'emailOrUsername'}` as Parameters<typeof setError>[0];
+        setError(field, {
+          message: err.message
+        });
+      });
+    }
+    if(data && data.login.accessToken) {
+      localStorage.setItem('access_token', data.login.accessToken);
+      navigate('/');
+    }
   }
 
   return (
@@ -62,7 +80,7 @@ function RealLoginForm(): React.ReactElement {
 
         <Divider/>
 
-        <Button colorScheme={"teal"} type={"submit"}>
+        <Button colorScheme={"teal"} type={"submit"} isLoading={loading}>
           로그인
         </Button>
       </Stack>
