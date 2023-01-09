8806aa8269ed58f62f0f876b4335dad433513499
bulhwi <mm225012@gmail.com>
2023-01-09 22:46:00 +0900

    apollo client link - onError, httpLink 설정 추가

--- graphql-book-fullstack-project/project/web/README.md
+++ graphql-book-fullstack-project/project/web/README.md
@@ -1,3 +1,4 @@
 * [GraphQL plugin](https://jimkyndemeyer.github.io/js-graphql-intellij-plugin/docs/developer-guide)
 * [아폴로 클라이언트에서 데이터 페이지네이션 처리 방식](https://www.apollographql.com/docs/react/pagination/overview)
 * [lazy load lib](https://www.npmjs.com/package/react-lazy-load-image-component) 
+* [Apollo Client Link - onError](https://www.apollographql.com/docs/react/api/link/apollo-link-error/)
--- graphql-book-fullstack-project/project/web/src/apollo/createApolloClient.ts
+++ graphql-book-fullstack-project/project/web/src/apollo/createApolloClient.ts
@@ -1,8 +1,31 @@
-import { ApolloClient, NormalizedCacheObject } from '@apollo/client';
+import {ApolloClient, from, HttpLink, NormalizedCacheObject} from '@apollo/client';
 import { createApolloCache } from "./createApolloCache";
+import {onError} from "@apollo/client/link/error";
+
+
+const errorLink = onError(({graphQLErrors, networkError, operation}) => {
+  if(graphQLErrors) {
+    graphQLErrors.forEach(({message, locations, path}) => {
+      // eslint-disable-next-line no-console
+      console.log(
+        `[GraphQL error]: -> ${operation.operationName}
+        Message: ${message}, Query: ${path}, Location: ${JSON.stringify(locations)}`,
+      )
+    })
+  }
+
+  if(networkError) {
+    // eslint-disable-next-line no-console
+    console.log(`[networkError]: -> ${operation.operationName} Message: ${networkError.message}`);
+  }
+});
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/graphql',
+});
 
 export const createApolloClient = (): ApolloClient<NormalizedCacheObject> =>
   new ApolloClient({
-    uri: "http://localhost:4000/graphql",
     cache: createApolloCache(),
+    link: from([errorLink, httpLink]),
   });
